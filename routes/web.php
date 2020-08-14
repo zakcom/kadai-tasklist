8a356d33e4798012b474b0e016f85b976bcf0d48
NAO <zackzakumi62@outlook.jp>
2020-08-14 05:01:26 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('/', 'TasksController@index');
 
 Route::resource('tasks','TasksController');
+
+//ユーザー登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+//ログイン、ログアウト
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
